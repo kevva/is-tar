011a94fa16a536a85777c9276953485480ace8c5
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-22 15:32:12 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,18 @@
+/*global describe, it */
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var isTar = require('../');
+var path = require('path');
+
+describe('extname()', function () {
+    function check(file) {
+        return isTar(fs.readFileSync(file));
+    }
+
+    it('should detect TAR from buffer', function (cb) {
+        assert(check(path.join(__dirname, 'fixtures/test.tar')));
+        cb();
+    });
+});
